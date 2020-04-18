6d96ae9ba6962375f582099c6f7b89ffc305fe2a
willclarktech <willclarktech@users.noreply.github.com>
2020-04-18 15:55:49 +0100

    refactor: rewrite source code in typescript

--- src/modules/protocol/rsa/index.js
+++
@@ -1,9 +0,0 @@
-import BigInteger from 'big-integer'
-import forge from 'node-forge'
-import { createModuleFactory } from '../../../factory'
-import module from './module'
-
-export default createModuleFactory({
-  forge: forge,
-  bigInt: BigInteger
-})(module)
